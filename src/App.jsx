5fc69effd9aca524c1c713446823a9b6d71efcd9
Daniel Rrapi <danielrrapi@proton.me>
2024-08-03 12:44:23 +0200

    Added BarLinks component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.scss";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import BarLinks from "./components/BarLinks.jsx";
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <BarLinks></BarLinks>
       <Hero></Hero>
     </>
   );
---
+++ src/components/BarLinks.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  position: fixed;
+  bottom: 0;
+  left: 50px;
+  &::after {
+    content: "";
+    width: 0;
+    height: 26vh;
+    border: 1px solid var(--lightest-navy);
+    display: block;
+    transform: translate(10px);
+  }
+`;
+
+const Svg = styled.img`
+  width: 20px;
+  height: 20px;
+  display: flex;
+  gap: 20px;
+  transition: 0.5s;
+  &:hover {
+    transform: translate(0, -5px);
+    transition: 0.5s;
+  }
+`;
+
+export default function BarLinks() {
+  return (
+    <Wrapper className="d-flex flex-column row-gap-4">
+      <a href="https://github.com/daniel-rrapi" target="_blank">
+        <Svg src="src/assets/svgs/github-logo.svg" alt="github logo" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/daniel-rrapi-full-stack-developer"
+        target="_blank"
+      >
+        <Svg src="src/assets/svgs/linkedin-logo.svg" alt="linkedin logo" />
+      </a>
+    </Wrapper>
+  );
+}
